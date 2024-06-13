ed8ee2dc7aa4880fb542fdcbcc701f2a93bcb8c2
jonathan <jonathan@MacBookAir.local>
2024-06-12 20:31:26 -0500

    Add action, dockerfile and start.sh

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM ghcr.io/hdiseguroscol/php-nginx-docker:v1.0.0 AS base
+
+WORKDIR /var/www/html/
+COPY . .
+
+FROM base AS files
+
+RUN mkdir -p docroot/sites/default/files
+RUN chmod 777 docroot/sites/default/files -R
+RUN chmod 777 docroot/sites/default/settings.php
+
+RUN mkdir -p efs
+RUN chown nginx:root efs
+RUN chmod 755 efs
+
+RUN mkdir -p docroot/sites/sponsors/files
+RUN chmod 777 docroot/sites/sponsors/files -R
+COPY start.sh /opt/start.sh
+
+FROM files AS final
+
+EXPOSE 80
+
+STOPSIGNAL SIGTERM
+
+RUN dos2unix /opt/start.sh
+
+ENTRYPOINT ["sh","/opt/start.sh"]
---
+++ start.sh
@@ -0,0 +1,26 @@
+cat /etc/os-release
+cat /etc/hostname
+cat /etc/resolv.conf
+
+printenv
+
+rm -rf /var/www/html/docroot/sites/default/files
+rm -rf /var/www/html/docroot/sites/sponsors/files
+
+ln -s /var/www/html/efs/files/default/files /var/www/html/docroot/sites/default/
+ln -s /var/www/html/efs/files/sponsors/files /var/www/html/docroot/sites/sponsors/
+
+echo "127.0.0.1 ${ListDns}" >> /etc/hosts
+
+cat /etc/hosts
+
+#Comandos para hacer el drush cr en tiempo de ejecucion
+echo "start drush commands"
+drush sync:import
+drush advagg-caf
+drush cr
+echo "finish drush commands"
+
+php-fpm81 && nginx -g 'daemon off;'
+
+echo "Fin start."
