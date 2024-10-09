1b3d3389b0962d7470fe0727d9b8bb9fef393673
Esquivel, Jonathan <Jonathan.Esquivel@Libertycolombia.com>
2024-10-09 09:55:01 -0500

    AITSSD-21602: Ajustes estilos topbar.

---
+++ docroot/themes/custom/hdi_portal/assets/js/chat.config.js
@@ -0,0 +1,1 @@
+!function(e,t){"use strict";t.behaviors.themeChatJS={attach:function(t,o){e(".closeButton").click((function(t){e(".embeddedServiceSidebarHeader").not("noAnimate")&&alert("va")})),console.log("ok")}}}(jQuery,Drupal,0,this.document);
---
+++ docroot/themes/custom/hdi_portal/assets/js/masonry.config.js
@@ -0,0 +1,1 @@
+!function(t,i){"use strict";i.behaviors.libertyMasonryConfig={attach:function(i,r){t(".grid").masonry({itemSelector:".grid-item",columnWidth:300})}}}(jQuery,Drupal,0,this.document);
