4d4ce033220a74d13c7a51600a2164050ded2e5e
Jonathan Esquivel <jonathan.esquivel@hdiseguros.com.co>
2025-12-18 11:51:26 -0500

    AITSSD-7208: Borrar temas antiguos sin uso.

--- docroot/themes/custom/liberty/dist/js/02-molecules/coverage/coverage.js
+++
@@ -1,9 +0,0 @@
-/******/ (function() { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!******************************************************!*\
-  !*** ./components/02-molecules/coverage/coverage.js ***!
-  \******************************************************/
-(function(a,b){b.behaviors.coverage={attach(b){a(".form-modal",b).click(function(){a(".form-modal-wrap").addClass("display")})}}})(jQuery,Drupal);
-/******/ })()
-;
-//# sourceMappingURL=coverage.js.map
--- docroot/themes/custom/liberty/templates/js/form-validate.js
+++
@@ -1,5 +0,0 @@
-function getIdButton (id){
-
-    let selector = document.getElementsByName('producto');
-    selector[0].value = id;
-  }
