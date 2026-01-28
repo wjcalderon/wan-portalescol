2c07889377f535f9e29970e263efb6ed0af6f8f4
Calderon Gutierrez <wendy.calderon@outsourcing.hdiseguros.com.co>
2026-01-28 15:19:41 -0500

    AITSSD-0-update: Upgrade de librerias en lib_red_medica.

--- docroot/modules/custom/lib_red_medica/app/vue.config.js
+++ docroot/modules/custom/lib_red_medica/app/vue.config.js
@@ -2,12 +2,5 @@ module.exports = {
   publicPath: "/modules/custom/lib_red_medica/app/dist",
   outputDir: 'dist',
   filenameHashing: false,
-  productionSourceMap: true,
-  css: {
-    loaderOptions: {
-      scss: {
-        prependData: `@import "@/assets/styles.scss";`,
-      },
-    },
-  },
+  productionSourceMap: true
 }
