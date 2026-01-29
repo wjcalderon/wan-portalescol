cbd20c1a9ab61474cc086cf4cb29d914a58276d1
Calderon Gutierrez <wendy.calderon@outsourcing.hdiseguros.com.co>
2026-01-29 15:17:19 -0500

    AITSSD-0-update: Eliminacion yarn.lock y ajuste de espacios en scss

--- docroot/themes/custom/hdi_portal/assets/js/chat.config.js
+++ docroot/themes/custom/hdi_portal/assets/js/chat.config.js
@@ -1,2 +1,2 @@
-!function(e,t){"use strict";t.behaviors.themeChatJS={attach:function(t,o){e(".closeButton").click((function(t){e(".embeddedServiceSidebarHeader").not("noAnimate")&&alert("va")})),console.log("ok")}}}(jQuery,Drupal,0,this.document);
+!function(e,t){"use strict";t.behaviors.themeChatJS={attach:function(t,o){e(".closeButton").click(function(t){e(".embeddedServiceSidebarHeader").not("noAnimate")&&alert("va")}),console.log("ok")}}}(jQuery,Drupal,0,this.document);
 //# sourceMappingURL=chat.config.js.map
